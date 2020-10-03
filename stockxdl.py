b3fc7fe63bf22c35346fe0289a01d5dd623b2578
000Nobody <65867770+000Nobody@users.noreply.github.com>
2020-10-02 21:09:14 -0400

    Update stockxdl.py

--- stockxdl.py
+++ stockxdl.py
@@ -7,30 +7,58 @@ import csv
 # Do you hate how I code? Fork and pull request. You won't, no balls.
 # @dogman_aud
 
-print('')
-print('StockX Data Downloader')
-print('by @dogman_aud')
-print('made better by you!')
-print('')
-sku = input('Input SKU (or search term, if you\'re feeling lucky): ')
-search = requests.get('https://stockx.com/api/search?query={}&currency=USD'.format(str(sku)))
-searchJSON = json.loads(search.text)
-firstSearch = searchJSON['hits'][0]
-name = firstSearch['url']
-objectid = firstSearch['objectID']
-saleRAW = requests.get('https://stockx.com/api/products/{}/activity?state=480&currency=USD&limit=100000000&page=1&sort=createdAt&order=DESC'.format(objectid))
-saleJSON = json.loads(saleRAW.text)
+def search(sku):
 
+    search = requests.get(
+            f'https://stockx.com/api/search?query={sku}&currency=USD'
+        )
 
-with open('{}.csv'.format(name),'w') as outputCSV:
-    csvWriter = csv.writer(outputCSV)
-    csvWriter.writerow(['Date','Size',"Price"])
-    for item in saleJSON['ProductActivity']:
-        csvWriter.writerow([item['createdAt'],item['shoeSize'],item['amount']])
-        # uncomment the following if you like terminal spam
-        # print('Wrote: Date: {} | Size: {} | Amount: {}'.format(item['createdAt'],item['shoeSize'],item['amount']))
+    searchJSON = json.loads(search.text)
+    firstSearch = searchJSON['hits'][0]
+    name = firstSearch['url']
+    objectid = firstSearch['objectID']
+    saleRAW = requests.get((
+                'https://stockx.com/api/products/{}'
+                '/activity?state=480&currency=USD'
+                '&limit=100000000&page=1&sort=createdAt&order=DESC'
+                .format(objectid)
+            )
+        )
 
-print('')
-print('Success: wrote {}.csv'.format(name))
-print('Total Sales: {}'.format(str(saleJSON['Pagination']['total'])))
-print('')
+    saleJSON = json.loads(saleRAW.text)
+
+    return saleJSON
+
+def toCSV(saleJSON):
+
+    with open(f'{name}.csv','w') as outputCSV:
+        csvWriter = csv.writer(outputCSV)
+        csvWriter.writerow(['Date','Size','Price'])
+        for item in saleJSON['ProductActivity']:
+            csvWriter.writerow([item['createdAt'],item['shoeSize'],item['amount']])
+        #   uncomment the following if you like terminal spam
+        #     print(
+        #     f'Wrote: Date: {item['createdAt']} | Size: {item['shoeSize']} | Amount: {item['amount']}'
+        # )
+
+def main():
+
+    print('')
+    print('StockX Data Downloader')
+    print('by @dogman_aud')
+    print('made better by you!')
+    print('')
+
+    sku = input('Input SKU (or search term, if you\'re feeling lucky): ')
+
+    saleJSON = search(str(sku))
+
+    toCSV(saleJSON)
+
+    print('')
+    print('Success: wrote {}.csv'.format(name))
+    print('Total Sales: {}'.format(str(saleJSON['Pagination']['total'])))
+    print('')
+
+if __name__ == '__main__':
+    main()
